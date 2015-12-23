5b4418c0d54d9cf7873a73bf32ea8456c7ed992b
Administrator <Administrator@wyyang>
2015-12-23 20:16:16 +0800

    1、请求服务器获取省级、市级列表 2、解析返回的省级数据

---
+++ src/com/coolweather/app/util/HttpUtil.java
@@ -0,0 +1,44 @@
+package com.coolweather.app.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * 通过请求网络接口服务，得到所有的城市列表
+ * @author Administrator
+ *
+ */
+public class HttpUtil {
+	public static void sendHttpRequest(final String address,final HttpCallbackListener listener){
+		HttpURLConnection connection = null;
+		try {
+			URL url = new URL(address);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setReadTimeout(8000);
+			connection.setConnectTimeout(8000);
+			InputStream in = connection.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			StringBuilder response = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null){
+				response.append(line);
+			}
+			if (listener != null){
+				listener.onFinish(response.toString());
+			}
+		} catch (IOException e) {
+			if (listener != null){
+				listener.onError(e);
+			}
+		} finally {
+			if (connection != null){
+				connection.disconnect();
+			}
+		}
+	}
+}
